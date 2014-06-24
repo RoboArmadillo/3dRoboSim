9088e2993606e410b874922315f7de75e9372d85
fergie57 <fergie57@btinternet.com>
2014-06-24 11:57:44 +0100

    Arena markers work, tokens a little wonky

--- sim.py
+++ sim.py
@@ -4,24 +4,14 @@ import Image
 
 
 im = Image.open('libkoki.png')  
-im2 = Image.open('floor.png') 
+#im2 = Image.open('floor.png') 
 
 tex = materials.texture(data=im, mapping='sign')
 
-lamp = local_light(pos=(200,1000,200), color=color.white)
+lamp = local_light(pos=(200,300,200), color=color.white)
 
 color.brown = (0.38,0.26,0.078)
-
-
-
-
-
-
-
-
-
-
-
+color.orange = (0.85,0.54,0.18)
 
 #width and height
 #0-400
@@ -34,11 +24,11 @@ WIDTH = 400
 HEIGHT = 50
 
 #creates arena
-arenafloor = box(pos=(0,0,0), size=(WIDTH,4,LENGTH), color=color.green), #material = tex2)
-areawall1 = box(pos=(-WIDTH/2,HEIGHT/2,0), size=(4,HEIGHT,LENGTH), color=color.green)
-areawall2 = box(pos=(WIDTH/2,HEIGHT/2,0), size=(4,HEIGHT,LENGTH), color=color.green)
-areawall3 = box(pos=(0,HEIGHT/2,-LENGTH/2), size=(WIDTH,HEIGHT,4), color=color.green)
-areawall4 = box(pos=(0,HEIGHT/2,LENGTH/2), size=(WIDTH,HEIGHT,4), color=color.green)
+arenafloor = box(pos=(0,0,0), size=(WIDTH,4,LENGTH), color=color.orange), #material = tex2)
+areawall1 = box(pos=(-WIDTH/2,HEIGHT/2,0), size=(4,HEIGHT,LENGTH), color=color.orange)
+areawall2 = box(pos=(WIDTH/2,HEIGHT/2,0), size=(4,HEIGHT,LENGTH), color=color.orange)
+areawall3 = box(pos=(0,HEIGHT/2,-LENGTH/2), size=(WIDTH,HEIGHT,4), color=color.orange)
+areawall4 = box(pos=(0,HEIGHT/2,LENGTH/2), size=(WIDTH,HEIGHT,4), color=color.orange)
 
 
 
@@ -46,29 +36,23 @@ areawall4 = box(pos=(0,HEIGHT/2,LENGTH/2), size=(WIDTH,HEIGHT,4), color=color.gr
 #the marker object creates one "Marker" that represents one paper marker that we stick on the side of a "token".
 #For the Boxes look at the "Token" Object
 class Marker(object):
-	def __init__(self,code,x,y,z,orientation,marker_type):
+	def __init__(self,code,x,y,z,axis_decider,marker_type):
 		global player_position
 		self.x = x
 		self.y = y
 		self.z = z
 		self.pos = vector(self.x,self.y,self.z)
-		self.orientation = orientation
+
+		self.axis = vector(int(axis_decider[0]),int(axis_decider[1]),int(axis_decider[2]))
 
 		self.marker_type = marker_type
 		if self.marker_type == "token marker":
 			self.size = 9
 		elif self.marker_type == "token arena":
 			self.size = 40
+		print self.axis
+		self.marker = box(pos=self.pos, size=(0.01,self.size,self.size), color=color.white,material=tex,axis = self.axis)
 
-		
-		if self.orientation == "end":
-			self.marker = box(pos=self.pos, size=(self.size,0.01,self.size), color=color.white,material=tex)
-
-		elif self.orientation == "side":
-			self.marker = box(pos=self.pos, size=(self.size,self.size,0.01), color=color.white,material = tex)
-
-		elif self.orientation == "other_side":
-			self.marker = box(pos=self.pos, size=(0.01,self.size,self.size), color=color.white, material = tex)
 
 
 		self.angle = 0
@@ -87,13 +71,12 @@ class Token(object):
 		self.size = 10
 		self.box = self.marker = box(pos=self.pos, size=(self.size,self.size,self.size), color=color.brown)
 
-
-		self.markers = [Marker(code,self.x-5,7,self.z,"other_side","token marker"),
-						Marker(code,self.x+5,7,self.z,"other_side","token marker"),
-						Marker(code,self.x,7,self.z-5,"side","token marker"),
-						Marker(code,self.x,7,self.z+5,"side","token marker"),
-						Marker(code,self.x,2,self.z,"end","token marker"),
-						Marker(code,self.x,12,self.z,"end","token marker")]
+		self.markers = [Marker(code,self.x-5,7,self.z,(-1,0,0),"token marker"),
+						Marker(code,self.x+5,7,self.z,(1,0,0),"token marker"),
+						Marker(code,self.x,7,self.z-5,(0,0,1),"token marker"),
+						Marker(code,self.x,7,self.z+5,(0,0,-1),"token marker"),
+						Marker(code,self.x,2,self.z,(0,1,0),"token marker"),
+						Marker(code,self.x,12,self.z,(0,-1,0),"token marker")]
 
 
 
@@ -104,15 +87,14 @@ class Token(object):
 		self.code = code
 
 
-
 class Robot(object):
 	def __init__(self,x,y,z):
 		self.x = x
 		self.y = y
 		self.z = z
-		#self.velocity = vector(0,0,0)
+		self.velocity = vector(1,2,0)
 		self.pos = vector(self.x,self.y,self.z)
-		robot = box(pos=self.pos, size=(5,5,5), color=color.blue)
+		self.box = box(pos=self.pos, size=(5,5,5), color=color.blue)
 
 
 def populate_walls(Tokens_per_wallx,Tokens_per_wallz):
@@ -126,19 +108,25 @@ def populate_walls(Tokens_per_wallx,Tokens_per_wallz):
 	while counter <=Tokens_per_wallx:
 		xposnew = xpos + (counter * spacingx)
 		if counter > 0:
-			box = Marker(2,xposnew,ypos,zpos-6,"side","token arena")
+			box = Marker(2,xposnew,ypos,zpos-6,(0,0,-1),"token arena")
 		counter +=1
 
 	while counter <=Tokens_per_wallx+Tokens_per_wallz:
 		zposnew = zpos - ((counter-Tokens_per_wallx) * spacingz)
 		if counter > Tokens_per_wallx:
-			box = Marker(2,xpos+4,ypos,zposnew,"other_side","token arena")
+			box = Marker(2,xpos+2,ypos,zposnew,(1,0,0),"token arena")
 		counter +=1
 
-	while counter <=Tokens_per_wallx+Tokens_per_wallz:
-		zposnew = zpos - ((counter-Tokens_per_wallx) * spacingz)
-		if counter > Tokens_per_wallx:
-			box = Marker(2,xpos+4,ypos,zposnew,"other_side","token arena")
+	while counter <=((Tokens_per_wallx*2)+Tokens_per_wallz):
+		xposnew = xpos + ((counter-Tokens_per_wallx-Tokens_per_wallz) * spacingz)
+		if counter > Tokens_per_wallx+Tokens_per_wallz:
+			box = Marker(2,xposnew+2,ypos,zpos-LENGTH,(0,0,1),"token arena")
+		counter +=1
+
+	while counter <=(Tokens_per_wallx+Tokens_per_wallz)*2:
+		zposnew = zpos - ((counter-Tokens_per_wallx-Tokens_per_wallz-Tokens_per_wallx) * spacingz)
+		if counter > Tokens_per_wallx+Tokens_per_wallz+Tokens_per_wallx:
+			box = Marker(2,xpos+WIDTH-2,ypos,zposnew,(-1,0,0),"token arena")
 		counter +=1
 
 	
@@ -158,7 +146,11 @@ def populate_walls(Tokens_per_wallx,Tokens_per_wallz):
 for x in xrange(41,50):
 	marker_list.append(Token(x))
 
+time.sleep(1)
 populate_walls(5,5)
+R = Robot(0,0,0)
+while True:
+	rate(24)
 
 
 
