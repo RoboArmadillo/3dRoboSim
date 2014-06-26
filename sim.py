05521b0721260714bd112292ff22e3e40e611fec
fergie57 <fergie57@btinternet.com>
2014-06-26 21:54:28 +0100

    added new movement code from Twoshanks

--- sim.py
+++ sim.py
@@ -24,6 +24,7 @@ player_position = vector(1,2,3)
 LENGTH = 400
 WIDTH = 400
 HEIGHT = 50
+RATE = 24
 
 #creates arena
 arenafloor = box(pos=(0,0,0), size=(4,WIDTH,LENGTH), color=color.orange, material = tex2, axis=(0,1,0))
@@ -34,63 +35,63 @@ areawall4 = box(pos=(0,HEIGHT/2,LENGTH/2), size=(WIDTH,HEIGHT,4), color=color.or
 
 
 def backward_map(x,in1,in2,out1,out2):
-	a=(in2-in1) #difference between inputs
-	b =(out2-out1) #difference between output
-	scale_factor = float(b)/float(a)
-	return out2 -x*scale_factor
+    a=(in2-in1) #difference between inputs
+    b =(out2-out1) #difference between output
+    scale_factor = float(b)/float(a)
+    return out2 -x*scale_factor
 
 
 #the marker object creates one "Marker" that represents one paper marker that we stick on the side of a "token".
 #For the Boxes look at the "Token" Object
 class Marker(object):
-	def __init__(self,code,x,y,z,axis_decider,marker_type):
-		global player_position
-		self.x = x
-		self.y = y
-		self.z = z
-		self.pos = vector(self.x,self.y,self.z)
+    def __init__(self,code,x,y,z,axis_decider,marker_type):
+        global player_position
+        self.x = x
+        self.y = y
+        self.z = z
+        self.pos = vector(self.x,self.y,self.z)
 
-		self.axis = vector(int(axis_decider[0]),int(axis_decider[1]),int(axis_decider[2]))
+        self.axis = vector(int(axis_decider[0]),int(axis_decider[1]),int(axis_decider[2]))
 
-		self.marker_type = marker_type
-		if self.marker_type == "token marker":
-			self.size = 9
-		elif self.marker_type == "token arena":
-			self.size = 40
-		self.marker = box(pos=self.pos, size=(0.01,self.size,self.size), color=color.white,material=tex,axis = self.axis)
+        self.marker_type = marker_type
+        if self.marker_type == "token marker":
+            self.size = 9
+        elif self.marker_type == "token arena":
+            self.size = 40
+        self.marker = box(pos=self.pos, size=(0.01,self.size,self.size), color=color.white,material=tex,axis = self.axis)
 
 
 
-		self.angle = 0
-		self.angle_rad = math.radians(self.angle)
-		self.distance = math.sqrt((self.pos.x -player_position.x)**2 + (self.pos.y -player_position.y)**2 + (self.pos.z -player_position.z)**2)
-		self.code = code
+        self.angle = 0
+        self.angle_rad = math.radians(self.angle)
+        self.distance = math.sqrt((self.pos.x -player_position.x)**2 + (self.pos.y -player_position.y)**2 + (self.pos.z -player_position.z)**2)
+        self.code = code
 
 
 
 class Token(object):
-	def __init__(self,code):
-		global player_position
-		self.x = random.randint((-WIDTH/2)+11,WIDTH/2-11)
-		self.z = random.randint((-LENGTH/2)+11,LENGTH/2-11)
-		self.pos = vector(self.x,7,self.z)
-		self.size = 10
-		self.box = self.marker = box(pos=self.pos, size=(self.size,self.size,self.size), color=color.brown)
+    def __init__(self,code):
+        global player_position
+        self.x = random.randint((-WIDTH/2)+11,WIDTH/2-11)
+        self.z = random.randint((-LENGTH/2)+11,LENGTH/2-11)
+        self.pos = vector(self.x,7,self.z)
+        self.size = 10
+        self.box = self.marker = box(pos=self.pos, size=(self.size,self.size,self.size), color=color.brown)
 
-		self.markers = [Marker(code,self.x-5,7,self.z,(-1,0,0),"token marker"),
-						Marker(code,self.x+5,7,self.z,(1,0,0),"token marker"),
-						Marker(code,self.x,7,self.z-5,(0,0,-1),"token marker"),
-						Marker(code,self.x,7,self.z+5,(0,0,1),"token marker"),
-						Marker(code,self.x,2,self.z,(0,-1,0),"token marker"),
-						Marker(code,self.x,12,self.z,(0,1,0),"token marker")]
+        self.markers = [Marker(code,self.x-5,7,self.z,(-1,0,0),"token marker"),
+                        Marker(code,self.x+5,7,self.z,(1,0,0),"token marker"),
+                        Marker(code,self.x,7,self.z-5,(0,0,-1),"token marker"),
+                        Marker(code,self.x,7,self.z+5,(0,0,1),"token marker"),
+                        Marker(code,self.x,2,self.z,(0,-1,0),"token marker"),
+                        Marker(code,self.x,12,self.z,(0,1,0),"token marker")]
 
 
 
-		self.angle = 0
-		self.angle_rad = math.radians(self.angle)
-		self.pos = vector(self.x,7,self.z)
-		#self.token.rotate(angle = self.angle_rad,axis=(0,1,0),origin = self.pos)
-		self.code = code
+        self.angle = 0
+        self.angle_rad = math.radians(self.angle)
+        self.pos = vector(self.x,7,self.z)
+        #self.token.rotate(angle = self.angle_rad,axis=(0,1,0),origin = self.pos)
+        self.code = code
 
 
 
@@ -109,7 +110,7 @@ class Motor(object):
 
     @speed.setter
     def speed(self, value):
-    	global speed
+        global speed
         self._speed = value
 
         
@@ -120,115 +121,114 @@ class Motor(object):
 
 
 class Robot(object):
-	def __init__(self,x,y,z):
-		self.x = x
-		self.y = y
-		self.z = z
-		self.velocity = vector(0,5,0)
-		self.pos = vector(self.x,self.y,self.z)
-		self.box = box(pos=self.pos, size=(30,30,50), color=color.blue)
-		self.motors=[Motor(0),Motor(1),Motor(2)]
-		self.heading = vector(0,0,1)
+    def __init__(self,x,y,z):
+        self.x = x
+        self.y = y
+        self.z = z
+        self.velocity = vector(0,5,0)
+        self.pos = vector(self.x,self.y,self.z)
+        self.box = box(pos=self.pos, size=(50,30,30), color=color.blue)
+        self.motors=[Motor(0),Motor(1),Motor(2)]
+        self.heading = vector(0,0,1)
 
 
 def populate_walls(Tokens_per_wallx,Tokens_per_wallz):
-	spacingx = WIDTH/(Tokens_per_wallx+1)
-	spacingz = LENGTH/(Tokens_per_wallz+1)
-	#xwall1
-	counter = 0
-	xpos = -WIDTH/2
-	ypos = HEIGHT/2
-	zpos = LENGTH/2+4
-	while counter <=Tokens_per_wallx:
-		xposnew = xpos + (counter * spacingx)
-		if counter > 0:
-			box = Marker(2,xposnew,ypos,zpos-6,(0,0,-1),"token arena")
-		counter +=1
+    spacingx = WIDTH/(Tokens_per_wallx+1)
+    spacingz = LENGTH/(Tokens_per_wallz+1)
+    #xwall1
+    counter = 0
+    xpos = -WIDTH/2
+    ypos = HEIGHT/2
+    zpos = LENGTH/2+4
+    while counter <=Tokens_per_wallx:
+        xposnew = xpos + (counter * spacingx)
+        if counter > 0:
+            box = Marker(2,xposnew,ypos,zpos-6,(0,0,-1),"token arena")
+        counter +=1
 
-	while counter <=Tokens_per_wallx+Tokens_per_wallz:
-		zposnew = zpos - ((counter-Tokens_per_wallx) * spacingz)
-		if counter > Tokens_per_wallx:
-			box = Marker(2,xpos+2,ypos,zposnew,(1,0,0),"token arena")
-		counter +=1
+    while counter <=Tokens_per_wallx+Tokens_per_wallz:
+        zposnew = zpos - ((counter-Tokens_per_wallx) * spacingz)
+        if counter > Tokens_per_wallx:
+            box = Marker(2,xpos+2,ypos,zposnew,(1,0,0),"token arena")
+        counter +=1
 
-	while counter <=((Tokens_per_wallx*2)+Tokens_per_wallz):
-		xposnew = xpos + ((counter-Tokens_per_wallx-Tokens_per_wallz) * spacingz)
-		if counter > Tokens_per_wallx+Tokens_per_wallz:
-			box = Marker(2,xposnew+2,ypos,zpos-LENGTH,(0,0,1),"token arena")
-		counter +=1
+    while counter <=((Tokens_per_wallx*2)+Tokens_per_wallz):
+        xposnew = xpos + ((counter-Tokens_per_wallx-Tokens_per_wallz) * spacingz)
+        if counter > Tokens_per_wallx+Tokens_per_wallz:
+            box = Marker(2,xposnew+2,ypos,zpos-LENGTH,(0,0,1),"token arena")
+        counter +=1
 
-	while counter <=(Tokens_per_wallx+Tokens_per_wallz)*2:
-		zposnew = zpos - ((counter-Tokens_per_wallx-Tokens_per_wallz-Tokens_per_wallx) * spacingz)
-		if counter > Tokens_per_wallx+Tokens_per_wallz+Tokens_per_wallx:
-			box = Marker(2,xpos+WIDTH-2,ypos,zposnew,(-1,0,0),"token arena")
-		counter +=1
+    while counter <=(Tokens_per_wallx+Tokens_per_wallz)*2:
+        zposnew = zpos - ((counter-Tokens_per_wallx-Tokens_per_wallz-Tokens_per_wallx) * spacingz)
+        if counter > Tokens_per_wallx+Tokens_per_wallz+Tokens_per_wallx:
+            box = Marker(2,xpos+WIDTH-2,ypos,zposnew,(-1,0,0),"token arena")
+        counter +=1
 
-	
+    
 
 time.sleep(1)
 populate_walls(5,5)
 R = Robot(0,17,0)
 
 for x in xrange(41,50):
-	marker_list.append(Token(x))
+    marker_list.append(Token(x))
 
 def velocity_checker():
-	counter = 0
-	while True:
-		rate(24)
-		#sitatuation if they are both going dead forwards
-		if R.motors[0].speed == R.motors[1].speed:
-			a = R.motors[0].speed
-			R.velocity = -R.heading*R.motors[0].speed*0.1
-			R.box.pos += R.velocity
-
-		#situation if they are going the same speed. but one forwards and backwards
-		elif R.motors[0].speed == -R.motors[1].speed: 
-			rot_speed = backward_map(abs(R.motors[0].speed),0,100,pi/20,pi/500)
-			R.box.rotate(angle=rot_speed, axis=vector(0,1,0), origin=R.box.pos)
-			v1 = R.heading
-			R.heading = rotate(v1, angle=rot_speed, axis=(0,1,0))
-
-		elif R.motors[0].speed and R.motors[1].speed >0 or R.motors[0].speed and R.motors[1].speed <0: #wonky needs fixing
-			difference = float(R.motors[0].speed - R.motors[1].speed)
-			value = 100.00/difference
-			multiplier = 30
-			#then I need to work out the axis on which we are working
-			b = vector(0,1,0) #rotation is about the y axis
-			perp = cross(R.heading,b)
-			 #gives the perpendicular vector
-			centre = R.box.pos - value*multiplier*norm(perp)
-			distance = mag(centre)
-
-			#turning speed slows as centre position is not updated not sure why..
-
-			if 	R.motors[0].speed > R.motors[1].speed and (R.motors[0].speed and R.motors[1].speed>0): #works
-				rot_speed =(-pi/30)/distance*min(R.motors[0].speed,R.motors[1].speed)
-				print distance
-				centre = R.box.pos - value*multiplier*norm(perp)
-
-			elif R.motors[0].speed > R.motors[1].speed and (R.motors[0].speed and R.motors[1].speed<0): #works
-				rot_speed =(-pi/30)/distance*min(R.motors[0].speed,R.motors[1].speed)
-				centre = R.box.pos + value*multiplier*norm(perp)
-
-			elif R.motors[0].speed < R.motors[1].speed and (R.motors[0].speed and R.motors[1].speed<0): 
-				rot_speed =(pi/30)/distance*min(R.motors[0].speed,R.motors[1].speed)
-				centre = R.box.pos + value*multiplier*norm(perp)
-
-
-			elif R.motors[0].speed < R.motors[1].speed and (R.motors[0].speed and R.motors[1].speed>0):
-				rot_speed =(pi/30)/distance*min(R.motors[0].speed,R.motors[1].speed)
-				centre = R.box.pos - value*multiplier*norm(perp)
+    
+    oldspeed1 = 1.0
+    oldspeed2 = 1.0
+    while True:
+        global R
+        if oldspeed1 != R.motors[0].speed or oldspeed2 != R.motors[1].speed:
+            oldspeed1 = R.motors[0].speed
+            oldspeed2 = R.motors[1].speed
 
 
 
+            pivot = vector(R.box.axis.z,0,-R.box.axis.x)
+            pivot = norm(pivot)
+            averagespeed = (abs(R.motors[0].speed) + abs(R.motors[1].speed))/2
+            speeddiff = (R.motors[0].speed - R.motors[1].speed)/100
 
-			
-			R.heading = rotate(R.heading, angle=rot_speed, axis=(0,1,0))
-			R.box.rotate(angle=rot_speed, axis=vector(0,1,0), origin=centre)
 
 
-			
+            if speeddiff == 0:
+                speeddiff += 0.000000001
+            if (R.motors[0].speed < 0.1 and R.motors[0].speed > -0.1):
+                speeddiff = 1
+            elif (R.motors[1].speed < 0.1 and R.motors[1].speed > -0.1):
+                speeddiff = -1
+
+
+            pivot = pivot * R.box.size.z/2 / speeddiff
+            radius = mag(pivot)
+
+
+            if (R.motors[0].speed > 0 and R.motors[1].speed < 0) or (R.motors[0].speed < 0 and R.motors[1].speed > 0):
+                pivot = R.box.pos #needs to be variable
+                radius = R.box.size.z
+
+
+            if R.motors[1].speed>R.motors[0].speed:
+                radius = radius*-1
+
+            if R.motors[0].speed < 0 and R.motors[1].speed < 0:
+                averagespeed = -averagespeed
+
+            print averagespeed
+
+
+            angularvelocity = averagespeed/radius
+        if R.box.pos.x > -WIDTH/2 and R.box.pos.x < WIDTH/2 and R.box.pos.z < LENGTH/2 and R.box.pos.z > -LENGTH/2:
+            R.box.rotate(angle=angularvelocity/RATE, axis=(0,1,0), origin = pivot)
+
+                
+        else:
+            print "crashed"
+
+        time.sleep(0.02)    
+
+            
 
 
 
@@ -239,19 +239,19 @@ counter =0
 left = R.motors[0]
 right = R.motors[1]
 while True:
-	rate(24)
-	left.speed = 100
-	right.speed = 50
-	time.sleep(2)
-	left.speed = 100
-	right.speed = 100
-	time.sleep(2)
+    rate(10)
+    left.speed = 100
+    right.speed = 100
+    time.sleep(2)
+    left.speed = 100
+    right.speed = 100
+    time.sleep(0.5)
 
-	'''
-	time.sleep(0.5)
-	R.motors[0].speed = 30
-	R.motors[1].speed = 30
-	time.sleep(1)
-	'''
-	
+    '''
+    time.sleep(0.5)
+    R.motors[0].speed = 30
+    R.motors[1].speed = 30
+    time.sleep(1)
+    '''
+    
 
